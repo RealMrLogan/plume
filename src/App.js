cdecace4de28625a15ca1116ec8d957c3bb9925c
Logan Saunders <lsaunders275@gmail.com>
2020-09-08 21:35:37 -0600

    more success

--- src/App.js
+++ src/App.js
@@ -1,11 +1,1 @@
-import React from 'react';
-import './App.css';
-import { Button } from './stories/Button/Button';
-
-export default () => {
-  return (
-    <div>
-      <Button>Hello World</Button>
-    </div>
-  )
-}
+export default null
--- src/stories/Button/Button.jsx
+++ src/stories/Button/Button.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import './button.css'
-import {css} from 'emotion'
+import { css } from 'emotion'
 
 const buttonCss = css`
   box-sizing: box-sizing;
@@ -14,24 +14,29 @@ const buttonCss = css`
   }
 `
 
-export const Button = ({children, priority}) => {
+export const Button = ({ children, priority }) => {
   let baseColor
   switch (priority) {
-    case "high":
-    baseColor = css`--base-color: var(--purple1);`
-    break;
-  default:
-    baseColor = css`--base-color: var(--purple2);`
+    case 'high':
+      baseColor = css`--base-color: var(--purple1);`
+      break
+    default:
+      baseColor = css`--base-color: var(--purple2);`
   }
 
   return (
-    <button css={[baseColor, buttonCss]}>
+    <button type="button" css={[baseColor, buttonCss]}>
       {children}
     </button>
   )
 }
-
 Button.propTypes = {
   children: PropTypes.node,
   priority: PropTypes.string,
 }
+Button.defaultProps = {
+  children: null,
+  priority: 'low',
+}
+
+export default null
